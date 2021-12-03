500477a6fdd8d893a772ba356a882e772595764c
Peter <warrenp11@gmail.com>
2021-12-03 16:58:52 -0500

    updated res.render to display data correctly on homepagE

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,8 +35,8 @@ router.get("/", (req, res) => {
   })
     .then((dbPostData) => {
       // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
-      res.render("homepage", dbPostData[0]);
+    //   console.log(dbPostData[0]);
+      res.render("homepage", dbPostData[0].get({ plain: true }));
     })
     .catch((err) => {
       console.log(err);
