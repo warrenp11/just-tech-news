9381afea3da9a46da739872b4c5285544b06c45f
Peter <warrenp11@gmail.com>
2021-12-03 16:56:09 -0500

    set up initial homepage functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,46 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
 // Homepage route for Handlebars
 router.get("/", (req, res) => {
-  res.render("homepage", {
-    id: 1,
-    post_url: "https://handlebarsjs.com/guide/",
-    title: "Handlebars Docs",
-    created_at: new Date(),
-    vote_count: 10,
-    comments: [{}, {}],
-    user: {
-      username: "test_user",
-    },
-  });
+    // same findAll in api/post-routes.js
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      res.render("homepage", dbPostData[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
