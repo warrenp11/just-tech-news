0cc879553727bfdb19f28bab5ed7e60cb8ad2948
Peter <warrenp11@gmail.com>
2021-12-02 19:31:42 -0500

    exclude password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,10 @@ const { User } = require("../../models");
 // GET /api/users
 router.get("/", (req, res) => {
   // Access our User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+      // exclude showing password
+      attributes: { exclude: ['password'] }
+  })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
@@ -15,6 +18,7 @@ router.get("/", (req, res) => {
 // GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id,
     },
