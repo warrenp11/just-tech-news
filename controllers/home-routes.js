81b4ffdac2450c5eeab232546394b1f361ec897d
Peter <warrenp11@gmail.com>
2021-12-03 16:53:40 -0500

    display hardcoded data to homepage.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+// Homepage route for Handlebars
+router.get("/", (req, res) => {
+  res.render("homepage", {
+    id: 1,
+    post_url: "https://handlebarsjs.com/guide/",
+    title: "Handlebars Docs",
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: "test_user",
+    },
+  });
 });
 
 module.exports = router;
