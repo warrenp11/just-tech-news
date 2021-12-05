dbbc2a10be704f2839767a2b67dddd639c0dfdb3
Peter <warrenp11@gmail.com>
2021-12-05 14:45:01 -0500

    upvote button and logic now functional

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -103,16 +103,22 @@ router.post("/", (req, res) => {
     });
 });
 
-// VOTE MODEL...vote on a post
+// UPVOTE MODEL...vote on a post
 // PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 // Update a post's title
