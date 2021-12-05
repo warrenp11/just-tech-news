3169d3d7e1ad3a620aa2232e1d9d0efca4fd9146
Peter <warrenp11@gmail.com>
2021-12-05 16:41:00 -0500

    test passing for pluralizing word

--- _tests_/helpers.test.js
+++ _tests_/helpers.test.js
@@ -1,4 +1,12 @@
-const {format_date} = require('../utils/helpers');
+const { format_date, format_plural } = require("../utils/helpers");
+
+test("format_plural() returns a pluralized word", () => {
+  const word1 = format_plural("tiger", 1);
+  const word2 = format_plural("lion", 2);
+
+  expect(word1).toBe("tiger");
+  expect(word2).toBe("lions");
+});
 
 test("format_date() returns a date string", () => {
   const date = new Date("2020-03-20 16:12:03");
