4e517acf756ce5a9c10dc7b7a20ff2f86480a407
Peter <warrenp11@gmail.com>
2021-12-05 15:52:11 -0500

    updated to conditionally display login/logout based on session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,7 +39,10 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       //   console.log(dbPostData[0]);
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts });
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
